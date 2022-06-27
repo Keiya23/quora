085daef8201513d9da351ac0267640dcf23f6443
Lambert TESSIER <lamberttessier@gmail.com>
2022-06-27 11:54:26 +0200

    Affichage de la liste des questions

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,31 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        $questions = [
+            [
+                'title' => 'Je suis une question',
+                'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, non!',
+                'rating' => 20,
+                'author' => [
+                    'name' => 'Sushi',
+                    'avatar' => 'https://randomuser.me/api/portraits/men/2.jpg'
+                ],
+                'nbReponse' => 15
+            ],
+            [
+                'title' => 'Je suis une question',
+                'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, non!',
+                'rating' => -15,
+                'author' => [
+                    'name' => 'Nem',
+                    'avatar' => 'https://randomuser.me/api/portraits/women/47.jpg'
+                ],
+                'nbReponse' => 12
+            ]
+            ];
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'questions' => $questions
         ]);
     }
 }
