87f4e22598f7fb7a9aca6f8e86bae6d69206550c
Lambert TESSIER <lamberttessier@gmail.com>
2022-07-19 09:39:20 +0200

    Mise en place de la page de profil de l'utilisateur lambda

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -76,8 +76,8 @@ class UserController extends AbstractController
         if($user === $currentUser) {
             return $this-> redirectToRoute('current_user');
         }
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'Profil d\'un Utilisateur',
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
         ]);
     }
 }
